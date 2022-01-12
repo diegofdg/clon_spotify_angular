bbbb75bc191ef430a1b08b16dd6ee7f1d9e7390b
diegofdg <diegofdg@gmail.com>
2022-01-12 15:43:36 -0300

    router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
-    path: '', // una cadena vacía representa http://localhost:4200/
+    path: 'auth',
+    loadChildren: ()=> import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    component: HomePageComponent,
     loadChildren: ()=> import('./modules/home/home.module').then(m => m.HomeModule)
   }
 ];
