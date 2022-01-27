0e749099976ec4ff0df1197e1e59e6ac3cd48f75
diegofdg <diegofdg@gmail.com>
2022-01-26 21:16:59 -0300

    api node - demo

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { TrackService } from '@modules/tracks/services/track.service';
 import { Subscription } from 'rxjs';
-import * as dataRaw from '../../../../data/tracks.json';
 
 @Component({
   selector: 'app-tracks-page',
@@ -18,24 +17,15 @@ export class TracksPageComponent implements OnInit, OnDestroy {
   constructor(private trackService: TrackService) { }
 
   ngOnInit(): void {
-    const observer1$ = this.trackService.dataTracksTrending$
+    this.trackService.getAllTracks$()
       .subscribe(response => {
-        this.tracksTrending = response;
-        this.tracksRandom = response;
-        console.log('canciones trending', response);      
-    });    
-
-    const observer2$ = this.trackService.dataTracksRandom$
-      .subscribe(response => {
-        this.tracksRandom = [... this.tracksRandom, ... response];
-        console.log('canciones random', response);      
-    });
-    this.listObservers$ = [observer1$, observer2$]
+        console.log(response);
+        
+    })
+    
   }
 
   ngOnDestroy(): void {
-    this.listObservers$.forEach(u => u.unsubscribe())
-    console.log('🔴🔴🔴🔴🔴🔴🔴 BOOM!');
 
   }
 }
--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,38 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TrackModel } from '@core/models/tracks.model';
-import { observable, Observable, of } from 'rxjs';
-import * as dataRaw from '../../../data/tracks.json';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TrackService {
-  dataTracksTrending$: Observable<TrackModel[]> = of([]);
-  dataTracksRandom$: Observable<any> = of([]);
+export class TrackService {  
+  private readonly URL = environment.api
 
-  constructor() {
-    const { data }: any = (dataRaw as any).default;
+  constructor(private httpClient: HttpClient) {    
 
-    this.dataTracksTrending$ = of(data);
+  }
 
-    this.dataTracksRandom$ = new Observable((observer) => {
-      const trackExample: TrackModel = {
-        _id: 9,
-        name: 'Leve',
-        album: 'Cartel de Santa',
-        url: 'http://',
-        cover: 'https://th.bing.com/th/id/OIP.oR_v894YBFhKgGD0bUyR0gHaHa?pid=ImgDet&rs=1'
-      }
-
-      setTimeout(() => {
-        observer.next([trackExample])
-      },3000)
-
-
-      
-
-
-    });
-
+  getAllTracks$(): Observable<any> {
+    return this.httpClient.get(`${this.URL}/tracks`)
   }
 }
