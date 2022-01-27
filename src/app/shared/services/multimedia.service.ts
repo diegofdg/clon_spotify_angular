fc776d5d59d688f3ea7b97e7f78a41eba592d0b8
diegofdg <diegofdg@gmail.com>
2022-01-27 18:30:01 -0300

    obtener tiempo transcurrido y restante

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -10,6 +10,8 @@ export class MultimediaService {
 
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
   public audio!: HTMLAudioElement;
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00');
 
   constructor() {
     this.audio = new Audio();
@@ -18,6 +20,40 @@ export class MultimediaService {
         this.setAudio(responseOk)
       }
     });
+
+    this.listenAllEvents();
+  }
+
+  private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false);    
+  }
+
+  private calculateTime = () => {
+    const { duration, currentTime } = this.audio;
+    this.setTimeElapsed(currentTime);
+    this.setRemaining(currentTime, duration);
+  }
+
+  private setTimeElapsed(currentTime: number): void {
+    let seconds = Math.floor(currentTime % 60);
+    let minutes = Math.floor((currentTime / 60) % 60);
+    
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `${displayMinutes}:${displaySeconds}`;
+    this.timeElapsed$.next(displayFormat);
+  }
+
+  private setRemaining(currentTime: number, duration: number): void {
+    let timeLeft = duration - currentTime;
+
+    let seconds = Math.floor(timeLeft % 60);
+    let minutes = Math.floor((timeLeft / 60) % 60);
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`;
+    this.timeRemaining$.next(displayFormat);
   }
 
   public setAudio(track: TrackModel): void {
