48244cb91e38a4527554d84a8bf5edf1b6d1cef0
diegofdg <diegofdg@gmail.com>
2022-01-27 11:25:33 -0300

    pipe async

--- src/app/modules/history/services/search.service.ts
+++ src/app/modules/history/services/search.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,5 +14,8 @@ export class SearchService {
 
   searchTracks$(term: string): Observable<any> {
     return this.http.get(`${this.URL}/tracks?src=${term}`)
+      .pipe(
+        map((dataRaw: any) => dataRaw.data)
+      )
   }
 }
