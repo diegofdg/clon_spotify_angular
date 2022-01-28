3d05301fec1ba0c0c60b744ae2813e66a5024827
diegofdg <diegofdg@gmail.com>
2022-01-28 19:56:24 -0300

    test login-page.component.ts

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginPageComponent } from './login-page.component';
@@ -27,5 +28,50 @@ describe('LoginPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /*** Metodo AAA (Arrange / Act / Assert) ***/
+
+  it('🔴 Deberia de retornar "invalido" el formulario', () => {
+    // Arrange
+    const mockCredentials = {
+      email: '0x0x0x0x0x0',
+      password: '1111111111111111111111111'
+    }
+
+    const emailForm: any = component.formLogin.get('email');
+    const passwordForm: any = component.formLogin.get('password');
+
+    //Act
+    emailForm.setValue(mockCredentials.email);
+    passwordForm.setValue(mockCredentials.password);
+
+    //Assert
+    expect(component.formLogin.invalid).toEqual(true);
+  });
+
+  it('✔✔ Deberia de retornar "valido" el formulario', () => {
+    // Arrange
+    const mockCredentials = {
+      email: 'test@test.com',
+      password: '12345678'
+    }
+
+    const emailForm: any = component.formLogin.get('email');
+    const passwordForm: any = component.formLogin.get('password');
+
+    // Act
+    emailForm.setValue(mockCredentials.email);
+    passwordForm.setValue(mockCredentials.password);
+
+    // Assert
+    expect(component.formLogin.invalid).toEqual(false);
+  });
+
+  it('👍 El boton deberia de tener la palabra "Iniciar sesión"', () => {
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'));
+    const getInnerText = elementRef.nativeElement.innerText;
+
+    expect(getInnerText).toEqual('Iniciar sesión');
   });
 });
